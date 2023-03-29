deb8deba178f3c6d7856f951ea0a8778500a51b5
gouravengineer <gouravenginner@gmail.com>
2023-03-30 04:19:04 +0530

    updated some form correct route and working in route

--- food_recommendation/routes.py
+++ food_recommendation/routes.py
@@ -5,19 +5,26 @@ from food_recommendation.model import User,Records,Orders
 from flask_bcrypt import Bcrypt
 bcrypt=Bcrypt()
 
+
+
+
 @app.route("/home",methods=('GET', 'POST'))
 @app.route("/",methods=('GET', 'POST'))
 def home():
-    if request.method == 'Post':
-        course_type = request.form['mood']
-        food_type = request.form['preference']
-        dry_or_gravy = request.form['type']
+    if request.method == 'POST':
+        print("hello")
+        course_type = request.form['course_type']
+        food_type = request.form['food_type']
+        dry_or_gravy = request.form['dry_or_gravy']
+        print(course_type,food_type,dry_or_gravy)
         with app.app_context():
             db.create_all()
             record=Records(course_type=course_type,food_type=food_type,dry_or_gravy=dry_or_gravy)
             db.session.add(record)
             db.session.commit()
-        return render_template('login.html',record_id=record.id)
+            record=record.to_dict()
+            record_id=record['id']
+        return redirect(url_for('login',record_id=record_id))
 
     return render_template('home.html')
 
@@ -26,26 +33,30 @@ def get_total_food():
         food_data = json.load(f)
     return food_data
 
+
 @app.route("/login", methods=('GET', 'POST'))
 def login():
-    if request.method == 'Post':
+    record_id=request.args.get('record_id')
+    if request.method == 'POST':
         error=None
         email = request.form['email']
         password = request.form['password']
+        record_id = request.form['record_id']
         user=User.query.filter_by(email=email).all()
         if len(user)==0:
             error=f"The {email} is not registered"
-            return render_template('login.html',error=error)
+            return render_template('login.html',error=error,record_id=record_id)
         else:
             hash_password=user[0].password
-            verify_password=bcrypt.check_password_hash(hash_password,password)
+            verify_password=bcrypt.check_password_hash(hash_password,password,record_id=record_id)
             if not verify_password:
                 error = "Password not verified"
-                return render_template('login.html',error=error)
+                return render_template('login.html',error=error,record_id=record_id)
             else:
+
                 return redirect(url_for('home'))
             
-    return render_template('login.html')
+    return render_template('login.html',record_id=record_id)
 
 @app.route("/signup",methods=('GET','POST'))
 def signup():
@@ -76,7 +87,17 @@ def signup():
         return redirect(url_for('home'))
     return render_template('signup.html',total_food=get_total_food())
 
-    
+@app.route("/render_signup",methods=('GET','POST'))
+def render_signup():
+    if request.method == 'POST':
+        record_id = request.form['record_id']
+        return render_template('signup.html',error=None,record_id=record_id)
+
+@app.route("/render_login",methods=('GET','POST'))
+def render_login():
+    if request.method == 'POST':
+        record_id = request.form['record_id']
+        return render_template('login.html',error=None,record_id=record_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
