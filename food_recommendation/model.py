7fb636deb972ed74bdc489e10bf35446dd8d68f6
gouravengineer <gouravenginner@gmail.com>
2023-03-30 04:19:04 +0530

    updated some form correct route and working in route

--- food_recommendation/model.py
+++ food_recommendation/model.py
@@ -8,13 +8,17 @@ class User(db.Model):
     city = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
 
     def __repr__(self):
         return f"User('{self.name}', '{self.email}', '{self.city}')"
 
 class Records(db.Model):
     id = db.Column(db.Integer, primary_key=True,unique=True,nullable=False)
-    user_id = db.Column(db.String(120), nullable=False)
+    user_id = db.Column(db.String(120), nullable=True)
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     course_type = db.Column(db.String(120), nullable=False)
     food_type = db.Column(db.String(120), nullable=False)
@@ -24,8 +28,11 @@ class Records(db.Model):
     suggested_restaurant = db.Column(db.String(120),nullable=True)
     restaurant_rating = db.Column(db.Integer, nullable=True)
 
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
     def __repr__(self):
-        return f"User('{self.id}', '{self.user_id}', '{self.timestamp}', '{self.suggested_food}', '{self.suggested_restaurant}')"
+        return f"Records('{self.id}', '{self.user_id}', '{self.timestamp}', '{self.suggested_food}', '{self.suggested_restaurant}')"
     
 class Orders(db.Model):
     id = db.Column(db.Integer, primary_key=True,unique=True,nullable=False)
@@ -35,5 +42,9 @@ class Orders(db.Model):
     food_type = db.Column(db.String(120), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
 
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
     def __repr__(self):
-        return f"User('{self.id}', '{self.user_id}', '{self.timestamp}', '{self.food_choice}', '{self.food_type}', '{self.rating}')"
+        return f"Orders('{self.id}', '{self.user_id}', '{self.timestamp}', '{self.food_choice}', '{self.food_type}', '{self.rating}')"
