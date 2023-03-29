94973b9d76c8eb52537dba8277921ff896a8ea29
gouravengineer <gouravenginner@gmail.com>
2023-03-29 21:02:48 +0530

    Records table class is created

--- food_recommendation/model.py
+++ food_recommendation/model.py
@@ -11,3 +11,18 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.name}', '{self.email}', '{self.city}')"
+
+class Records(db.Model):
+    id = db.Column(db.Integer, primary_key=True,unique=True,nullable=False)
+    user_id = email = db.Column(db.String(120), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    course_type = db.Column(db.String(120), nullable=False)
+    food_type = db.Column(db.String(120), nullable=False)
+    dry_or_gravy = db.Column(db.String(120), nullable=False)
+    suggested_food = db.Column(db.String(120),nullable=True)
+    rating_of_food = db.Column(db.Integer, nullable=True)
+    suggested_restaurant = db.Column(db.String(120),nullable=True)
+    restaurant_rating = db.Column(db.Integer, nullable=True)
+
+    def __repr__(self):
+        return f"User('{self.id}', '{self.user_id}', '{self.timestamp}', '{self.timestamp}', '{self.suggested_food}', '{self.suggested_restaurant}')"
